46ea1e55c7c167f607422651f742cec79780be05
AprioriFox <apriorifox@gmail.com>
2021-12-17 17:53:18 +0600

    push all tasks

--- DZ2/main.js
+++ DZ2/main.js
@@ -1,55 +1,64 @@
 //First task
 const container = document.getElementById(`container`)
-const addElement = () =>{
+const addElement = () => {
     const div = document.createElement(`div`)
     div.setAttribute(`class`, `element`)
-    div.innerText="Element"
-
+    div.textContent="Element"
     container.appendChild(div)
 }
 //Second task
-const stop = document.getElementById(`stop`)
-const start = document.getElementById(`start`)
+
 const container2 = document.getElementById(`container2`)
 const max = 20;
 const min = 0;
-
 const elem = () => {
     const elem1 = document.createElement(`div`)
     elem1.setAttribute(`class`, `element`)
-    elem1.innerHTML = Math.floor(Math.random()*(max-min+1)+min);
+    elem1.innerHTML = Math.floor(Math.random()*20);
     container2.appendChild(elem1);
 }
-interval = setInterval(elem,5000)
+let interval = setInterval(elem,1000)
 const startInterval = () => {
-        interval = setInterval(elem,5000)
+        interval = setInterval(elem,1000)
 }
 const stopInterval = () => {
     clearInterval(interval)
 }
 
-
 //Third task
 const minutes = document.getElementById(`minutes`)
 const seconds = document.getElementById(`seconds`)
+const goBtn = document.getElementById(`start_tt`)
+const stopBtn = document.getElementById(`pause`)
+const nullBtn = document.getElementById(`reset`)
 
 const timer = () => {
     seconds.textContent = (+seconds.textContent + 1).toString().padStart(2, `0`);
-    if (+seconds.textContent === 60){
-        minutes.textContent = (+minutes.textContent + 1).toString().padStart(2, "0");
+    if (seconds.textContent === `60`){
+        minutes.textContent = (+minutes.textContent + 1).toString().padStart(2, `0`);
         seconds.textContent = `00`
     }
 }
 const startBtn = () => {
     strtInt = setInterval(timer,1000)
+    goBtn.disabled = true;
+    stopBtn.disabled = false;
+    nullBtn.disabled = false;
+
 }
 const pauseBtn = () => {
     clearInterval(strtInt)
+    goBtn.disabled = false;
+    stopBtn.disabled = true;
+    nullBtn.disabled = false;
 }
 const resetBtn = () => {
     clearInterval(strtInt)
     minutes.textContent = `00`
     seconds.textContent = `00`
+    goBtn.disabled = false;
+    stopBtn.disabled=true;
+    nullBtn.disabled=true;
 }
 //Forth task
 const user = document.forms[0];
@@ -62,10 +71,10 @@ const check = () => {
         logIn.disabled = false;
     }
 }
-    const log = () => {
-        alert(`Username = ${user.username.value} \n Password = ${user.password.value}`)
-    }
-
-
+const log = () => {
+    alert(`Username = ${user.username.value} \n Password = ${user.password.value}`)
+}
 user.addEventListener(`change`, check)
 logIn.addEventListener(`click`, log)
+
+
