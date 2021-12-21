7533cbd31cf06e3ed9f5f130116488fff0211938
AprioriFox <apriorifox@gmail.com>
2021-12-21 23:08:04 +0600

    bla bla bla

--- DZ3/index.js
+++ DZ3/index.js
@@ -1,19 +1,46 @@
-const input = document.querySelector('input')
-const name = document.getElementById('name')
-const region = document.getElementById('region')
-const subregion = document.getElementById('subregion')
-const capital = document.getElementById('capital')
-const flag = document.getElementById('flag')
+const input = document.querySelector('input'),
+    name = document.getElementById('name'),
+    region = document.getElementById('region'),
+    subregion = document.getElementById('subregion'),
+    capital = document.getElementById('capital'),
+    flag = document.getElementById('flag')
 const inputVal = () =>{
-    if (input.value.length > 0) {
-        fetch(`https://restcountries.com/v3.1/name/${input.value}`).then((data) => {
-            data.json().then(response => {
-                name.innerText = response[0].name.official;
-                region.innerText = response[0].region;
-                subregion.innerText = response[0].subregion;
-                capital.innerText = response[0].capital;
-                flag.innerHTML = `<img src="${response[0].flags.png}"/>`;
-            })
+    fetch(`https://restcountries.com/v3.1/name/${input.value}`).then((data) => {
+        data.json().then(response => {
+            name.innerText = response[0].name.official;
+            region.innerText = response[0].region;
+            subregion.innerText = response[0].subregion;
+            capital.innerText = response[0].capital;
+            flag.innerHTML = `<img src="${response[0].flags.png}"/>`;
+            console.log(response[0])
         })
-    }
+    })
 }
+const inpVal = () => {
+    fetch('https://restcountries.com/v3.1/all').then((date) => {
+        date.json().then(resp => {
+            console.log(resp.forEach(resp.name.common))
+        })
+    })
+}
+inpVal()
+// const inputVal = () =>{
+//     fetch('https://restcountries.com/v3.1/all').then((date) => {
+//         date.json().then(resp => {
+//             if (input.value == resp.forEach(name.common)){
+//                fetch(`https://restcountries.com/v3.1/name/${input.value}`).then((data) => {
+//                     data.json().then(response => {
+//                         name.innerText = response[0].name.official;
+//                         region.innerText = response[0].region;
+//                         subregion.innerText = response[0].subregion;
+//                         capital.innerText = response[0].capital;
+//                         flag.innerHTML = `<img src="${response[0].flags.png}"/>`;
+//                         console.log(response[0])
+//                     })
+//                 })
+//             }else{
+//                 alert('404 не найденно')
+//             }
+//         })
+//     })
+// }
