24b18048054bc0ae477e78cfc9c8e71627de017b
AprioriFox <apriorifox@gmail.com>
2021-12-21 21:41:58 +0600

    almost compled second level of first task

--- DZ3/index.js
+++ DZ3/index.js
@@ -4,7 +4,7 @@ const region = document.getElementById(`region`)
 const subregion = document.getElementById(`subregion`)
 const capital = document.getElementById(`capital`)
 const flag = document.getElementById(`flag`)
-const inputVal = (e) =>{
+const inputVal = () =>{
     if (input.value.length > 0) {
         fetch(`https://restcountries.com/v2/name/${input.value}`).then((data) => {
             data.json().then(response => {
@@ -12,15 +12,10 @@ const inputVal = (e) =>{
                 region.innerText = response[0].region;
                 subregion.innerText = response[0].subregion;
                 capital.innerText = response[0].capital;
-                const flagIcon = document.createElement(`img`)
-                flagIcon.setAttribute(`src`, response[0].flags.png)
-                flag.append(flagIcon)
-                console.log(input.value.length)
+                flag.innerHTML = `<img src="${response[0].flags.png}"/>`;
+                console.log(response[0].status)
             })
         })
-    }else{
-        alert(`404 страна не найдена`)
-        console.log(input.value.length)
     }
 }
 
