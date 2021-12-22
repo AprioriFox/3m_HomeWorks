2cd7d89439a984bddcfb55322e0855a9c352d0a1
AprioriFox <apriorifox@gmail.com>
2021-12-22 19:19:44 +0600

    comlieted first task

--- DZ3/index.js
+++ DZ3/index.js
@@ -13,34 +13,8 @@ const inputVal = () =>{
             capital.innerText = response[0].capital;
             flag.innerHTML = `<img src="${response[0].flags.png}"/>`;
             console.log(response[0])
+        }).catch(e => {
+            alert("Not found")
         })
     })
 }
-const inpVal = () => {
-    fetch('https://restcountries.com/v3.1/all').then((date) => {
-        date.json().then(resp => {
-            console.log(resp.forEach(resp.name.common))
-        })
-    })
-}
-inpVal()
-// const inputVal = () =>{
-//     fetch('https://restcountries.com/v3.1/all').then((date) => {
-//         date.json().then(resp => {
-//             if (input.value == resp.forEach(name.common)){
-//                fetch(`https://restcountries.com/v3.1/name/${input.value}`).then((data) => {
-//                     data.json().then(response => {
-//                         name.innerText = response[0].name.official;
-//                         region.innerText = response[0].region;
-//                         subregion.innerText = response[0].subregion;
-//                         capital.innerText = response[0].capital;
-//                         flag.innerHTML = `<img src="${response[0].flags.png}"/>`;
-//                         console.log(response[0])
-//                     })
-//                 })
-//             }else{
-//                 alert('404 не найденно')
-//             }
-//         })
-//     })
-// }
